045ccc2cb103c236e820f762326a47c41cbf0d9a
oswaldo <91742210+oswaldo-deco@users.noreply.github.com>
2023-10-14 15:51:24 -0300

    trabalho finalizado

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,24 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Por favor, digite o número da Numero !");
+        int numero = sc.nextInt();
+
+        // utilizei essa linha para receber o \n enviado pelo numero
+        String agencia = sc.nextLine();
+
+        System.out.println("Por favor, digite o número da Agência !");
+        agencia = sc.nextLine();
+
+        System.out.println("Por favor, digite o número da Nome Cliente !");
+        String nome = sc.nextLine();
+        
+        System.out.println("Por favor, digite o número da Saldo !");
+        double saldo = sc.nextDouble();
+
+        System.out.println("Olá "+ nome + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia +", conta " + numero + " e seu saldo " + saldo + " já está disponível para saque");
     }
 }
